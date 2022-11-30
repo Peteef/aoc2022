7d596b003743afe52e81c9edac748b63ef3149e0
kamil.turek <kamil.turek@ocado.com>
2022-11-30 17:32:18 +0100

    Initial commit

---
+++ src/main/kotlin/io/peteef/aoc/App.kt
@@ -0,0 +1,8 @@
+package io.peteef.aoc
+
+object App {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("/test_hello.dat".readFile())
+    }
+}
---
+++ src/main/kotlin/io/peteef/aoc/util.kt
@@ -0,0 +1,10 @@
+package io.peteef.aoc
+
+import java.io.File
+import java.io.InputStream
+
+fun String.asFileStream(): InputStream = App::class.java.getResourceAsStream(this) ?: File(this).inputStream()
+
+fun String.readFileLines(): List<String> = this.asFileStream().bufferedReader().readLines()
+
+fun String.readFile(): String = this.asFileStream().bufferedReader().readText()
