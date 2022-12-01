6d01c30371f159eacdce15a7d658f4c3f3bcc91d
kamil.turek <kamil.turek@ocado.com>
2022-12-01 23:57:52 +0100

    Day 1

--- src/main/kotlin/io/peteef/aoc/solutions/day1.kt
+++ src/main/kotlin/io/peteef/aoc/solutions/day1.kt
@@ -1,9 +1,21 @@
 package io.peteef.aoc.solutions
 
 import io.peteef.aoc.DescribedSolution
+import io.peteef.aoc.readFile
 
 val DAY1: DescribedSolution = "Day 1" to ::solve
 
 private fun solve() {
-    println("Solution 1")
+    val rawInput = "/input/input_1_1.dat".readFile()
+
+    val calories = rawInput.split("\n\n")
+        .map { it.split("\n").filter(String::isNotEmpty).sumOf(String::toInt) }
+
+    val max = calories.max()
+    println("Max calories is $max")
+
+    val threeHighestCombined = calories.sorted()
+        .takeLast(3)
+        .sum()
+    println("Three highest have combined $threeHighestCombined")
 }
